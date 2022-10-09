b1720d6b89c2915130adb8b81cc9d2ac71e5eeba
sidhansukeshri <112798263+sidhansukeshri@users.noreply.github.com>
2022-10-09 23:03:32 +0530

    Create 15.average of two numbers.c

---
+++ 15.average of two numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    int number1, number2;
+    float avg;
+    printf("Enter the First Number to find = ");
+    scanf("%d",&number1);
+    printf("Enter the Second Number to find  = ");
+    scanf("%d",&number2);
+    int sum = number1 + number2;
+    avg = sum/2; 
+    printf("The Sum of %d and %d     = %d\n", number1, number2, sum);
+    printf("The Average of %d and %d = %.2f\n", number1, number2, avg);
+    return 0;
+}
