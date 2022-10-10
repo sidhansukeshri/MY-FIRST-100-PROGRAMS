8f49d0b9d6db986be66255c151dfa42c0ec7c801
sidhansukeshri <112798263+sidhansukeshri@users.noreply.github.com>
2022-10-10 23:02:20 +0530

    Create 44.pyramid 10.c

---
+++ 44.pyramid 10.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+main()
+{ 
+    int c=1;
+    for(int a=5;a>0;a--)
+    {
+        for(int b=1;b<=a;b++)
+        {
+            printf("%d  ",c);
+            c++;
+        }
+        printf("\n");
+    }
+}
