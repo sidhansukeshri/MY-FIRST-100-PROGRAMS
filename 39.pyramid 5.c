5ac9bfadfb59db994a629a6fca3be97a9c7738d2
sidhansukeshri <112798263+sidhansukeshri@users.noreply.github.com>
2022-10-10 22:57:01 +0530

    Create 39.pyramid 5.c

---
+++ 39.pyramid 5.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+main()
+{ 
+    int c=1;
+    for(int a=1;a<5;a++)
+    {
+        for(int b=1;b<=a;b++)
+        {
+            printf("%d",c);
+            c++;
+        }
+        printf("\n");
+    }
+}
