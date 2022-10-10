11133718b0fb16c68c5db2c6ff992568fb672f17
sidhansukeshri <112798263+sidhansukeshri@users.noreply.github.com>
2022-10-10 22:07:19 +0530

    Create 27.good day.c

---
+++ 27.good day.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+  int time = 0;
+  printf("enter the time (value must be between 1-24): ");
+  scanf("%d",&time);
+  if (time < 10) {
+    printf("Good morning.");
+  } else if (time < 20) {
+    printf("Good day.");
+  } else {
+    printf("Good evening.");
+  }
+  return 0;
+}
