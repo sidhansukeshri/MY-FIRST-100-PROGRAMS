41958210ffe6e3d6f0ba115ded4e3cb0de665247
sidhansukeshri <112798263+sidhansukeshri@users.noreply.github.com>
2022-10-11 23:32:17 +0530

    Create 98.floyd's traingle.c

---
+++ 98.floyd's traingle.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main() {
+    int i, j, rows, counter;
+    printf("Enter the number of rows of Floyd's triangle\n");
+    scanf("%d", &rows);
+    for (counter = 1, i = 1; i <= rows; i++) {
+        for (j = 1; j <= i; j++) {
+            printf("%3d", counter++);
+        }
+        printf("\n");
+    }
+    getch();
+    return 0;
+}
