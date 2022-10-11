cc9eddbd34c3b4a825709eb93dd25842a7ee39bc
sidhansukeshri <112798263+sidhansukeshri@users.noreply.github.com>
2022-10-11 22:31:39 +0530

    Create 74.triangle patten.c

---
+++ 74.triangle patten.c
@@ -0,0 +1,15 @@
+#include<stdio.h>  
+int main()   
+{   
+  int row, col, space=5, s;  
+  for( row = 9; row >= 1;row-=2 ) 
+  {
+  	for(s=1; s<=space;s++)
+  	   printf(" ");
+  	for(col=1; col<=row; col++)
+	   printf("*");   
+  	printf("\n");
+  	space++;
+  }
+  return 0;
+}
