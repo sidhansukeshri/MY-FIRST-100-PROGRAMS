93334202cb61f598d7deb657f7e9014d2f3dd55a
sidhansukeshri <112798263+sidhansukeshri@users.noreply.github.com>
2022-10-11 23:34:38 +0530

    Create 99.find third angle of the triangle.c

---
+++ 99.find third angle of the triangle.c
@@ -0,0 +1,10 @@
+#include <stdio.h>  
+#define ANGLE_SUM 180
+int main() {
+    float a1, a2, a3;  
+    printf("Enter Two Angles of a Triangle\n");  
+    scanf("%f %f", &a1, &a2);  
+    a3 = ANGLE_SUM - (a1 + a2);  
+    printf("Third Angle = %0.4f", a3);
+    return 0;  
+}
