baaf78cbbbfbd06a858e3dd615daa4dfda94c358
sidhansukeshri <112798263+sidhansukeshri@users.noreply.github.com>
2022-10-11 23:10:16 +0530

    Create 89.sum of prime numbers till n.c

---
+++ 89.sum of prime numbers till n.c
@@ -0,0 +1,20 @@
+#include <stdio.h>  
+   int main() {  
+    int counter, N, i, isPrime, primeFactorSum = 0;    
+    printf("Enter a Number\n");  
+    scanf("%d", &N);   
+    for(counter = 2; counter <= N; counter++) {
+     isPrime = 1;
+        for(i = 2; i <=(counter/2); ++i) {
+            if(counter%i==0) {
+                isPrime = 0;
+                break;
+            }
+        }
+    
+        if(isPrime==1)
+            primeFactorSum += counter;
+    }
+    printf("Sum of Prime Numbers between 1 to %d : %d", N,primeFactorSum);
+    return 0;  
+}
