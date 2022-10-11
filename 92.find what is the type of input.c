d5f063e6332ba97c513a4e26107a406b244299ad
sidhansukeshri <112798263+sidhansukeshri@users.noreply.github.com>
2022-10-11 23:18:22 +0530

    Create 92.find what is the type of input.c

---
+++ 92.find what is the type of input.c
@@ -0,0 +1,15 @@
+#include <stdio.h>  
+   
+int main() {  
+    char character;
+    printf("Enter a Character\n");  
+    scanf("%c", &character);  
+    if((character >='a' && character <='z')||(character >='A' && character <='Z')){  
+        printf("%c is an Alphabet\n", character);  
+    } else if(character >= '0' && character <= '9') {
+        printf("%c is a Digit \n", character);  
+    } else {
+        printf("%c is a Graphical Character\n", character);  
+    }
+    return 0;  
+}
