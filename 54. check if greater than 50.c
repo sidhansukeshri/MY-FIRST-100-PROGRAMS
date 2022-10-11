b0465dd2aa4fdfdf9371b04849b86477bf7d2137
sidhansukeshri <112798263+sidhansukeshri@users.noreply.github.com>
2022-10-11 21:31:17 +0530

    Create 54. check if greater than 50.c

---
+++ 54. check if greater than 50.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    printf("enter number : ");
+    int a;
+    scanf("%d",&a);
+    if(a>50)
+    {
+        printf("greater than 50");
+    }
+    else
+    {
+        printf("less than 50");
+    }
+}
