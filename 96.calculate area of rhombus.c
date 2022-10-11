f9cde2d4af9cf05d170fd19d073be306230cf0e8
sidhansukeshri <112798263+sidhansukeshri@users.noreply.github.com>
2022-10-11 23:28:12 +0530

    Create 96.calculate area of rhombus.c

---
+++ 96.calculate area of rhombus.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <conio.h>
+ 
+int main(){
+    float diagonalOne, diagonalTwo, area;
+    printf("Enter the length of diagonals of rhombus\n");
+    scanf("%f %f", &diagonalOne, &diagonalTwo);
+    /* Area of rhombus = (product of diagonals)/2 
+                       = (diagonalOne X diagonalTwo)/2 */
+    area = (diagonalOne * diagonalTwo)/2;
+    printf("Area of rhombus : %0.4f\n", area);
+     
+    getch();
+    return 0;
+}
