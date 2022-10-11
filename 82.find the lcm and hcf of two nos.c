ef78e00590db9d584a8e5077437ea4cf13e62690
sidhansukeshri <112798263+sidhansukeshri@users.noreply.github.com>
2022-10-11 22:51:56 +0530

    Create 82.find the lcm and hcf of two nos.c

---
+++ 82.find the lcm and hcf of two nos.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main()
+{
+ int a,b,hcf,lcm,max,min,r;
+
+ printf("Enter two numbers:");
+ scanf("%d%d",&a,&b);    
+ if(a>b)
+  {
+   max=a;
+   min=b;
+  }
+   else
+    if(b>a)
+    {
+     max=b;
+     min=a;
+    }    
+    if(a==b)
+     hcf=a;
+    else
+    {
+     do
+     {
+      r=max%min;
+      max=min;
+      min=r;
+     }
+     while(r!=0);        
+      hcf=max;
+   }    
+   lcm=(a*b)/hcf;    
+   printf(" LCM=%d HCF=%d",lcm,hcf);
+   return 0;
+}
