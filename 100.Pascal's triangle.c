a3bee0fc1804d64591efc58bdb77d40118d9b96c
sidhansukeshri <112798263+sidhansukeshri@users.noreply.github.com>
2022-10-12 10:16:36 +0530

    Update 100.Pascal's triangle.c

--- 100.Pascal's triangle.c
+++ 100.Pascal's triangle.c
@@ -1,3 +1,12 @@
+int getFactorial(int N){
+    if(N < 0){
+        printf("Invalid Input: factorial not defined for negative numbers\n");
+        return 0;}
+    int nFactorial = 1, counter;    
+    for(counter = 1; counter <= N; counter++){
+        nFactorial = nFactorial * counter;}    
+    return nFactorial;}
+
 #include <stdio.h>  
 int getFactorial(int n);  
 int main() {  
@@ -9,20 +18,6 @@ int main() {
             printf("  ");  
         for(i = 0; i <= row; i++) {  
             value = getFactorial(row)/(getFactorial(i)*getFactorial(row-i));  
-            printf("%4d", value);  
-        }  
-        printf("\n");  
-    }  
-    return 0;  
-}  
-int getFactorial(int N){
-    if(N < 0){
-        printf("Invalid Input: factorial not defined for \
-negative numbers\n");
-        return 0;
-    }
-    int nFactorial = 1, counter;    for(counter = 1; counter <= N; counter++){
-        nFactorial = nFactorial * counter;
-    }    
-    return nFactorial;
-}
+            printf("%4d", value);}  
+        printf("\n");}  
+    return 0;}  
