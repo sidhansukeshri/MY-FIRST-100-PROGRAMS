a493953e8283bc626190c4fa15b10fbe41842cc3
sidhansukeshri <112798263+sidhansukeshri@users.noreply.github.com>
2022-10-17 10:56:09 +0530

    Update 10.enter a int and print.c

--- 10.enter a int and print.c
+++ 10.enter a int and print.c
@@ -2,8 +2,9 @@
 int main()
 {
     int testInteger;
+    printf("My regeistration number is RA2211042010032 !");
     printf("Enter an integer: ");
-    scanf("%d", &testInteger);  
+    scanf("%d", &testInteger); 
     printf("Number = %d",testInteger);
     return 0;
 }
