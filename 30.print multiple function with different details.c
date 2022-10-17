2ec6ade641ba6b15fbc7c7fcde5921514ffc5862
sidhansukeshri <112798263+sidhansukeshri@users.noreply.github.com>
2022-10-17 11:10:02 +0530

    Update 30.print multiple function with different details.c

--- 30.print multiple function with different details.c
+++ 30.print multiple function with different details.c
@@ -3,6 +3,7 @@ void myFunction(char name[], int age) {
   printf("Hello %s. You are %d years old\n", name, age);
 }
 int main() {
+  printf("My regeistration number is RA2211042010032 !");
   myFunction("sid", 18);
   myFunction("jassan", 19);
   myFunction("dubey", 20);
