fa2a578ea3e195d4337880eaa268d6f8ad75f186
sidhansukeshri <112798263+sidhansukeshri@users.noreply.github.com>
2022-10-17 10:57:48 +0530

    Update 11.cube of a number.c

--- 11.cube of a number.c
+++ 11.cube of a number.c
@@ -1,6 +1,7 @@
 #include<stdio.h>    
 int main(){    
-int number;    
+int number;  
+ printf("My regeistration number is RA2211042010032 !");
 printf("enter a number:");    
 scanf("%d",&number);    
 printf("cube of number is:%d ",number*number*number);    
