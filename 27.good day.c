5d5817e96655f99ac8d238ef238a245d90975400
sidhansukeshri <112798263+sidhansukeshri@users.noreply.github.com>
2022-10-17 11:07:44 +0530

    Update 27.good day.c

--- 27.good day.c
+++ 27.good day.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 int main() {
+  printf("My regeistration number is RA2211042010032 !");
   int time = 0;
   printf("enter the time (value must be between 1-24): ");
   scanf("%d",&time);
