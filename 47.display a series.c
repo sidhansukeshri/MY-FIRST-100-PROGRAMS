e53ad474666e1a2bae18bf02ff361822ba0cf4ca
sidhansukeshri <112798263+sidhansukeshri@users.noreply.github.com>
2022-10-18 08:50:01 +0530

    Update 47.display a series.c

--- 47.display a series.c
+++ 47.display a series.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-int main(){
+int main(){printf("My regeistration number is RA2211042010032 !");
     int n;
     printf("enter a number of terms (enter value between 1-9): ");
     scanf("%d",&n);
