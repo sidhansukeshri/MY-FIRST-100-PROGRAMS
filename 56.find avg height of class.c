d99805af5403affcdd7bc29f646c97dd9045fdc7
sidhansukeshri <112798263+sidhansukeshri@users.noreply.github.com>
2022-10-18 08:55:30 +0530

    Update 56.find avg height of class.c

--- 56.find avg height of class.c
+++ 56.find avg height of class.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 main()
-{
+{ printf("My regeistration number is RA2211042010032 !");
   float sum=0,height[10];
   printf("Enter height of all 10 students one by one in cm : ");
   for(int a=0;a<10;a++)
