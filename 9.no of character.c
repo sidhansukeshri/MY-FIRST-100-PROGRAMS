bd36f42314e9b257611ddec31fae6bba92364d10
sidhansukeshri <112798263+sidhansukeshri@users.noreply.github.com>
2022-10-29 20:31:27 +0530

    Update 9.no of character.c

--- 9.no of character.c
+++ 9.no of character.c
@@ -1,5 +1,6 @@
+//this code prints the no of characters in Hello!
 #include <stdio.h>
-int main() {printf("My regeistration number is RA2211042010032 !");
+int main() {printf("My regeistration number is RA2211042010032 !\n");
    int len = 0;
 len = printf ("Hello\n");
 printf ("Length: %d\n", len);
