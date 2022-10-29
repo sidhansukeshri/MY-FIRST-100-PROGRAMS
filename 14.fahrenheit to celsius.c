be841b1e07d629b83c3f77ee0354465e44312c29
sidhansukeshri <112798263+sidhansukeshri@users.noreply.github.com>
2022-10-29 20:45:28 +0530

    Update 14.fahrenheit to celsius.c

--- 14.fahrenheit to celsius.c
+++ 14.fahrenheit to celsius.c
@@ -1,7 +1,8 @@
+//this code prints the temparature given in fahrenheit into celsius!
 #include<stdio.h>
 int main(){
     float fahrenheit, celsius;
-    printf("My regeistration number is RA2211042010032 !");
+    printf("My regeistration number is RA2211042010032 !\n");
     printf("Enter Temperature Value (in Fahrenheit): ");
     scanf("%f", &fahrenheit);
     celsius = (fahrenheit-32)/1.8;
