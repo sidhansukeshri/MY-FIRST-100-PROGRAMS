92283912f17185840b1059e3458484918d03e9cd
sidhansukeshri <112798263+sidhansukeshri@users.noreply.github.com>
2022-10-29 20:50:54 +0530

    Update 19.print nos 1 to 10000000000.c

--- 19.print nos 1 to 10000000000.c
+++ 19.print nos 1 to 10000000000.c
@@ -1,6 +1,7 @@
+//this code prints the numbers form 1 to 1000000 through for loop !
 #include<stdio.h>
 int main(){
-      printf("My regeistration number is RA2211042010032 !");
+      printf("My regeistration number is RA2211042010032 !\n");
       int i=1;
       for(i=1;i<=10000000000;i++)
       {
