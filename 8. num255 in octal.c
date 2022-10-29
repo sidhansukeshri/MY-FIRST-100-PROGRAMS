626d163199bb7f50373168888458598ab1b728e3
sidhansukeshri <112798263+sidhansukeshri@users.noreply.github.com>
2022-10-29 20:29:08 +0530

    Update 8. num255 in octal.c

--- 8. num255 in octal.c
+++ 8. num255 in octal.c
@@ -1,5 +1,6 @@
+//this code prints the value of 255 in octal form!
 #include <stdio.h>
-int main() {printf("My regeistration number is RA2211042010032 !");
+int main() {printf("My regeistration number is RA2211042010032 !\n");
    int num = 255;
    printf("num in octal format: %o\n", num);
 return 0;
