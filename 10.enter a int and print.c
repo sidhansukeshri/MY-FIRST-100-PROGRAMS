148925401deeff81a1aa771e35a7d27800acf279
sidhansukeshri <112798263+sidhansukeshri@users.noreply.github.com>
2022-10-29 20:32:26 +0530

    Update 10.enter a int and print.c

--- 10.enter a int and print.c
+++ 10.enter a int and print.c
@@ -1,8 +1,9 @@
+//this code prints the integer provided by the user!
 #include <stdio.h>
 int main()
 {
     int testInteger;
-    printf("My regeistration number is RA2211042010032 !");
+    printf("My regeistration number is RA2211042010032 !\n");
     printf("Enter an integer: ");
     scanf("%d", &testInteger); 
     printf("Number = %d",testInteger);
