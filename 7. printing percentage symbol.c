34f23f5e346ecb4e78bba7c2a4f4c642a3d35eaf
sidhansukeshri <112798263+sidhansukeshri@users.noreply.github.com>
2022-10-29 20:27:25 +0530

    Update 7. printing percentage symbol.c

--- 7. printing percentage symbol.c
+++ 7. printing percentage symbol.c
@@ -1,5 +1,6 @@
+//this code prints the percentage symbol!
 #include <stdio.h>
-int main() {printf("My regeistration number is RA2211042010032 !");
+int main() {printf("My regeistration number is RA2211042010032 !\n");
    printf ("Hey I got 96.69%% in my final exams...yayyy!\n");
    return 0;
 }
