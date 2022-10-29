0c572ff24f8610b3ae2e20d903ad19cb8eafbf96
sidhansukeshri <112798263+sidhansukeshri@users.noreply.github.com>
2022-10-29 20:52:36 +0530

    Update 20 divide two nos.c

--- 20 divide two nos.c
+++ 20 divide two nos.c
@@ -1,6 +1,7 @@
+//this code devides two  numbers x and y !
 #include <stdio.h>
 int main() {
-  printf("My regeistration number is RA2211042010032 !");
+  printf("My regeistration number is RA2211042010032 !\n");
   int x = 12;
   int y = 3;
   printf("%d", x / y);
