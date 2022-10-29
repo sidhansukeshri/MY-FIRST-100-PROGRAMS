2af8315625ba6ee3999a7fbfe3aebf1da9fea276
sidhansukeshri <112798263+sidhansukeshri@users.noreply.github.com>
2022-10-29 20:21:42 +0530

    Update 3. Product of two floating nos.c

--- 3. Product of two floating nos.c
+++ 3. Product of two floating nos.c
@@ -1,6 +1,7 @@
+//this code prints the product of two floating numbers!
 #include <stdio.h>
 int main() {
-    printf("My regeistration number is RA2211042010032 !");
+    printf("My regeistration number is RA2211042010032 !\n");
     double a, b, product;
     printf("Enter two numbers: ");
     scanf("%lf %lf", &a, &b);  
