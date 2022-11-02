4c1de5292038381b214a9b2e8dfbe6cb3dcdf0d2
sidhansukeshri <112798263+sidhansukeshri@users.noreply.github.com>
2022-11-02 23:03:12 +0530

    Update 41. pyramid 7.c

--- 41. pyramid 7.c
+++ 41. pyramid 7.c
@@ -1,8 +1,10 @@
+//this code prints a pyramid patten using for loop and numbers !
+//my reg no is RA2211042010032
 #include<stdio.h>
 main()
-{ printf("My regeistration number is RA2211042010032 !");
+{ printf("My regeistration number is RA2211042010032 !\n");
     int c=1;
-    for(int a=1;a<5;a++)
+    for(int a=0;a<5;a++)
     {
         for(int b=1;b<=a;b++)
         {
